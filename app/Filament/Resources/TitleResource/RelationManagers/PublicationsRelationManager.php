e218832435688c6a7850f4f8e99308b9b049e708
gilr <gilles.richardot@free.fr>
2024-05-04 22:34:30 +0200

    v2b7: (filament) Attachement d'un titre à une publication (recherche discriminante, champs pivots)

--- app/Filament/Resources/TitleResource/RelationManagers/PublicationsRelationManager.php
+++ app/Filament/Resources/TitleResource/RelationManagers/PublicationsRelationManager.php
@@ -7,8 +7,11 @@ use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Tables\Actions\AttachAction;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\PublicationResource;
+use App\Models\Publication;
 
 class PublicationsRelationManager extends RelationManager
 {
@@ -16,27 +19,85 @@ class PublicationsRelationManager extends RelationManager
 
     public function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-            ]);
+        if ($form->getOperation() === 'edit')
+        {
+            return $form
+                ->schema([
+                Forms\Components\TextInput::make('order')
+                    ->label ('ordre de l\'élément dans l\'ouvrage.')
+                    ->numeric()
+                    ->required(),
+                Forms\Components\TextInput::make('start_page')
+                    ->label ('Numéro de première page du titre ou début du texte.')
+                    ->helperText('Indiquer le numéro de page du titre s\'il est une ou plusieurs pages avant le début de texte.')
+                    ->maxLength(8),
+                Forms\Components\TextInput::make('end_page')
+                    ->label ('Numéro de dernière page du texte.')
+                    ->maxLength(8),
+                Forms\Components\TextInput::make('level')
+                    ->label ('"Niveau" de l\'élément')
+                    ->helperText('"0.0" pour l\'équivalent à la publication elle même. "1.0" pour un texte, collection ou section de premier niveau. "2.0" pour un élément de second niveau, etc...')
+                    ->numeric()
+                    ->default(0)
+                    ->required(),
+                ]);
+        }
+        else
+        {
+            // Re-utilisation de la form de vue/modification
+            return PublicationResource::form($form);
+        }
+
     }
 
     public function table(Table $table): Table
     {
         return $table
-            ->recordTitleAttribute('name')
+//            ->recordTitleAttribute('name')
+            ->recordTitle(fn (Publication $record): string => "{$record->name} - {$record->type->getLabel()} ({$record->publisher->name} {$record->approximate_parution})")
+            ->paginated(false)
             ->columns([
-                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nom'),
+                Tables\Columns\TextColumn::make('type')
+                    ->label('Type de contenu'),
+                Tables\Columns\TextColumn::make('level')
+                    ->label('Niveau'),
+                Tables\Columns\TextColumn::make('order')
+                    ->label('Ordre'),
+                Tables\Columns\TextColumn::make('start_page')
+                    ->label('Page début'),
+                Tables\Columns\TextColumn::make('end_page')
+                    ->label('Page fin'),
             ])
             ->filters([
                 Tables\Filters\TrashedFilter::make()
             ])
             ->headerActions([
                 Tables\Actions\CreateAction::make(),
-                Tables\Actions\AttachAction::make(),
+                Tables\Actions\AttachAction::make()
+                    ->recordSelectSearchColumns(['name'])
+                    ->form(fn (AttachAction $action): array => [
+                        $action->getRecordSelect(),
+                        Forms\Components\TextInput::make('order')
+                            ->label ('ordre de l\'élément dans l\'ouvrage.')
+                            ->helperText('L\'ordre commence à 1')
+                            ->numeric()
+                            ->required(),
+                        Forms\Components\TextInput::make('start_page')
+                            ->label ('Numéro de première page du titre ou début du texte.')
+                            ->helperText('Indiquer le numéro de page du titre s\'il est une ou plusieurs pages avant le début de texte')
+                            ->maxLength(8),
+                        Forms\Components\TextInput::make('end_page')
+                            ->label ('Numéro de dernière page du texte')
+                            ->maxLength(8),
+                        Forms\Components\TextInput::make('level')
+                            ->label ('"Niveau" de l\'élément')
+                            ->helperText('"0.0" pour l\'équivalent à la publication elle même. "1.0" pour un texte, collection ou section de premier niveau. "2.0" pour un élément de second niveau, etc...')
+                            ->numeric()
+                            ->default(0)
+                            ->required(),
+                ])
             ])
             ->actions([
                 Tables\Actions\ViewAction::make(),
